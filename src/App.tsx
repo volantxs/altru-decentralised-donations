ddce73d69d022739fcf382f1c744264b11eea924
volantxs <volantxs@gmail.com>
2023-12-30 18:49:24 +0530

    asfsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+//lambda function?
+
+import {WsProvider, ApiPromise} from "@polkadot/api"
+import { useEffect, useState } from "react"
+
+const App = () => { 
+  const [api, setApi] = useState<ApiPromise>();
+
+  const setup = async () => {
+    const wsProvider = new WsProvider("wss://rpc.polkadot.io");
+    const api = await ApiPromise.create({provider: wsProvider});
+    setApi(api);
+  }
+
+  useEffect(() => {
+    setup();
+  }, [])
+
+  useEffect(() => {
+    if(!api) return
+
+    (async() => {
+      const time = await api.query.timestamp.now();
+      console.log(time);
+    })
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      dappj
     </>
   )
 }
